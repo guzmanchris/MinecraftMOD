43aae8b5461daa928af08c90cde265462dc1885a
Christian Guzman <christian.guzman1@upr.edu>
2018-11-23 23:24:45 -0400

    Changed Mango Wood Ore properties

--- src/main/java/tameAHorseMod/blocks/MangoWoodOre.java
+++ src/main/java/tameAHorseMod/blocks/MangoWoodOre.java
@@ -9,8 +9,8 @@ public class MangoWoodOre extends BlockBase {
 	public MangoWoodOre(String name, Material material) {
 		super(name, material);
 		setSoundType(SoundType.WOOD);
-		setHardness(2.5f);
-		setResistance(10.0f);
-		setHarvestLevel("pickaxe",1);
+		setHardness(1.5f);
+		setResistance(15.0f);
+		setHarvestLevel("axe",1);
 	}
 }
